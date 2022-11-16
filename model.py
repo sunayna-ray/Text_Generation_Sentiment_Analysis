424f6d1f17878582c89c17557da7803c416a560d
sunayna <30575613+sunayna-ray@users.noreply.github.com>
2022-11-15 22:03:43 -0600

    Perplexity Evaluation

--- model.py
+++ model.py
@@ -1,6 +1,10 @@
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
-from datasets import Dataset
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, get_linear_schedule_with_warmup
+from datasets import Dataset, load_dataset
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler, random_split
 import numpy as np
+import torch
+from tqdm import tqdm
+import random
 
 def get_transformer_model():
 
@@ -92,8 +96,40 @@ class TransformerModel(object):
 		"""
 
 		##### Your code here #####
-		
-		score = 100.0
+		tokenizer = self.tokenizer
+
+		test = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
+		encodings = self.tokenizer("\n\n".join(test["text"]), return_tensors="pt")
+
+		max_length = self.model.config.n_positions
+		stride = 512
+		seq_len = encodings.input_ids.size(1)
+
+		nlls = []
+		prev_end_loc = 0
+		for begin_loc in tqdm(range(0, seq_len, stride)):
+			end_loc = min(begin_loc + max_length, seq_len)
+			trg_len = end_loc - prev_end_loc  # may be different from stride on last loop
+			input_ids = encodings.input_ids[:, begin_loc:end_loc]
+			target_ids = input_ids.clone()
+			target_ids[:, :-trg_len] = -100
+
+			with torch.no_grad():
+				outputs = self.model(input_ids, labels=target_ids)
+
+				# loss is calculated using CrossEntropyLoss which averages over input tokens.
+				# Multiply it with trg_len to get the summation instead of average.
+				# We will take average over all the tokens to get the true average
+				# in the last step of this example.
+				neg_log_likelihood = outputs.loss * trg_len
+
+			nlls.append(neg_log_likelihood)
+
+			prev_end_loc = end_loc
+			if end_loc == seq_len:
+				break
+
+		score = torch.exp(torch.stack(nlls).sum() / end_loc)
 
 		##### Code done #####
 
